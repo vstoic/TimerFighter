f76fa7f15cd4554804fedbdc48618cd7ee94cc66
Sing Cheng <Sing.cheng@outlook.com>
2023-01-28 03:51:56 -0500

    fixed mute, reset, changed song

--- src/index.js
+++ src/index.js
@@ -3,34 +3,40 @@ import Sprite from './scripts/Sprite.js'
 import { Howl } from 'howler';
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    let playing = false;
     const canvas = document.getElementById('timer-fighter');
     const c = canvas.getContext('2d')
     const song = new Howl({
-      src: ["src/assets/Audio/Logic_Super_Mario_World.mp3"],
-      onplayerror: function () {
-        song.once("unlock", function () {
-          song.play();
-        });
-      },
+        src: ["src/assets/Audio/WaveMeow-Cosmic(cut).mp3"],
+        volume: 0.3,
+        onplayerror: function () {
+            song.once("unlock", function () {
+                // song.play();
+            });
+        },
     });
     canvas.width = 1024;
     canvas.height = 576;
-    // debugger
     let game = new Game(c, canvas, canvas.width, canvas.height);
     game.animateGame();
     game.animatePlayer();
     game.animateBot();
     // create model function for character select and time select
-    game.decreaseTimer(60);
-    song.play();
+    game.decreaseTimer(10);
+    // song.play();
     // document.querySelector(".music").addEventListener("click", () => {
     //     music.overworld.play();
     // });
     document.querySelector(".music").addEventListener("click", () => {
-        song.pause();
+        if (song.playing()) {
+            song.pause();
+        } else {
+        
+            song.play();
+        }
     });
     document.querySelector(".restart").addEventListener("click", () => {
-        ;
+        location.reload();
+        
     })
 })
